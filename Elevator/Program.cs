06c610a9e0cf0f90b0bd39a575e29e8b22e79b71
JLChnToZ <jeremylam619@gmail.com>
2020-02-10 15:28:57 +0800

    Remove testing flow could cause build error

--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -6,7 +6,6 @@ namespace Elevator {
             try {
                 PluginLoader.LoadPlugins();
                 var handler = new ParsedInfo(args).GetHandler();
-                HandlerMeta.CleanUp();
                 Environment.Exit(handler.Launch());
             } catch(Exception ex) {
                 Console.WriteLine($"Error: {ex.Message}");
